00ed6e97d4f6edd1452a6a1dfdf1a9bed2c95fda
TayyabShabbir07 <146079411+TayyabShabbir07@users.noreply.github.com>
2023-10-03 15:04:56 +0500

    update

--- index.html/main.js
+++ index.html/main.js
@@ -1,65 +1,81 @@
-let dropdown = document.querySelector(".drop-down-box-telet-box");
-let telentBtn = document.querySelector(".telent-box");
-let telentIcon = document.querySelector(".icon-box-telent-box")
-let on = 0;
-let mainHtml = document.querySelector("main");
+function telenboxDropDown() {
+    let dropdown = document.querySelector(".drop-down-box-telet-box");
+    let telentBtn = document.querySelector(".telent-box");
+    let telentIcon = document.querySelector(".icon-box-telent-box")
+    let on = 0;
+    let mainHtml = document.querySelector("main");
 
-// let headerHtml = document.querySelector("header")
+    // let headerHtml = document.querySelector("header")
 
-telentBtn.addEventListener("click", function () {
-    if (on == 0) {
-        telentBtn.style.backgroundColor = "white"
-        telentBtn.style.border = "3px solid  #13544e"
-        dropdown.style.display = "block"
-        telentIcon.style.transform="Rotate(180deg)"
-        
-        
-        on = 1
+    telentBtn.addEventListener("click", function () {
+        if (on == 0) {
+            telentBtn.style.backgroundColor = "white"
+            telentBtn.style.border = "2px solid  #13544e"
+            dropdown.style.display = "block"
+            
+           
 
 
-    } else {
-        searchBox.classList.remove("active")
+            on = 1
+
+
+        } else {
+
+            telentBtn.style.border = "none"
+            dropdown.style.display = "none"
+            on = 0
+
+        }
+
+    })
+    mainHtml.addEventListener("click", () => {
         telentBtn.style.border = "none"
         dropdown.style.display = "none"
         on = 0
-
-    }
-
-})
-mainHtml.addEventListener("click", () => {
-    telentBtn.style.border = "none"
-    dropdown.style.display = "none"
-    on = 0
-    searchBox.classList.remove("active")
+       
 
 
-})
+    })
+
+}
+telenboxDropDown();
 
 
-let scrollContainer = document.querySelector(".three-boxs-section-nine");
-let backBtn = document.querySelector(".left-icon");
-let nextBtn = document.querySelector(".right-icon");
+
+function slider() {
+    let scrollContainer = document.querySelector(".three-boxs-section-nine");
+    let backBtn = document.querySelector(".left-icon");
+    let nextBtn = document.querySelector(".right-icon");
 
 
 
 
-backBtn.addEventListener("click", () => {
-    scrollContainer.scrollLeft -= 1000
-    scrollContainer.style.scrollBehavior = "smooth"
-    backBtn.style.display = "none"
-    nextBtn.style.display = "block"
-});
-nextBtn.addEventListener("click", () => {
-    scrollContainer.scrollLeft += 1000
-    scrollContainer.style.scrollBehavior = "smooth"
-    nextBtn.style.display = "none"
-    backBtn.style.display = "block"
-});
+    backBtn.addEventListener("click", () => {
+        scrollContainer.scrollLeft -= 1000
+        scrollContainer.style.scrollBehavior = "smooth"
+        backBtn.style.display = "none"
+        nextBtn.style.display = "block"
+    });
+    nextBtn.addEventListener("click", () => {
+        scrollContainer.scrollLeft += 1000
+        scrollContainer.style.scrollBehavior = "smooth"
+        nextBtn.style.display = "none"
+        backBtn.style.display = "block"
+    });
 
-let searchBox = document.querySelector(".search-box");
-searchBox.addEventListener("click" ,()=>{
-    searchBox.classList.toggle("active")
-})
+
+}
+slider();
+
+function SearchBox() {
+    let inputBox = document.querySelector(".input-box");
+    inputBox.addEventListener("click", () => {
+        inputBox.classList.toggle("active")
+    })
+}
+SearchBox();
+
+
 
 
 
